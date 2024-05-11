3aceb065ceaab094c96ec9c9e0526a0c86a8599b
Abdul Rafay Khan <160124493+rktech0078@users.noreply.github.com>
2024-05-11 01:58:00 -0700

    GIAIC Project-07 (1st commit)

---
+++ main.js
@@ -0,0 +1,172 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+class player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    ;
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+    fuelIncrease() {
+        this.fuel = 100;
+    }
+}
+;
+class opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+}
+;
+let player1 = await inquirer.prompt([
+    {
+        name: "player",
+        type: "input",
+        message: "Enter Your Name: "
+    }
+]);
+console.log(chalk.bgYellow.whiteBright(`\n Hello "${player1.player}"! we are glad to see you in this game \n`));
+// **************************************************************************************************************//
+let opponent1 = await inquirer.prompt([
+    {
+        name: "opponent",
+        type: "list",
+        message: "Select your opponent to war with him: ",
+        choices: ['Batsmen', 'Spidermen', 'Hulk']
+    }
+]);
+console.log(chalk.bgYellow.whiteBright(`\n You Select "${opponent1.opponent}" as an opponent to fighting \n`));
+let p1 = new player(player1.player);
+let o1 = new opponent(opponent1.opponent);
+do {
+    // FOR BATS-MEN
+    if (opponent1.opponent === 'Batsmen') {
+        let ask = await inquirer.prompt([
+            {
+                name: "opponent",
+                type: "list",
+                message: "What would you like to do: ",
+                choices: ['Attack', 'Increase Fuel', 'Run for your life']
+            }
+        ]);
+        if (ask.opponent === 'Attack') {
+            let randomNum = Math.floor(Math.random() * 2);
+            if (randomNum > 0) {
+                p1.fuelDecrease();
+                console.log(chalk.bgGreen.white(`\n "${p1.name}" Fuel is "${p1.fuel}"`));
+                console.log(chalk.bgGreen.white(`"${o1.name}" Fuel is "${o1.fuel}" \n`));
+                if (p1.fuel <= 0) {
+                    console.log(chalk.bgBlue.white(`\n Game Over "${player1.player}"! Better Luck Next Time.... \n Thank You for playing`));
+                    process.exit();
+                }
+            }
+            if (randomNum <= 0) {
+                o1.fuelDecrease();
+                console.log(chalk.bgGreen.white(`\n ${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bgGreen.white(`${o1.name} fuel is ${o1.fuel} \n`));
+                if (o1.fuel <= 0) {
+                    console.log(chalk.bgBlue.white(`\n Congratulations "${player1.player}"! You are winner.... \n Thank You for playing \n`));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opponent === 'Increase Fuel') {
+            p1.fuelIncrease();
+            console.log(chalk.bgMagenta.white(`\n You succesfully drink the "ENERGY BOOSTER", Now your strength is "${p1.fuel}" \n`));
+        }
+        if (ask.opponent === 'Run for your life') {
+            console.log(chalk.bgRed.white("\n You Quit this game! Better Luck Next Time, \n Thank You For Choosing us"));
+            process.exit();
+        }
+    }
+    // FOR SPIDER-MEN
+    if (opponent1.opponent === 'Spidermen') {
+        let ask = await inquirer.prompt([
+            {
+                name: "opponent",
+                type: "list",
+                message: "What would you like to do: ",
+                choices: ['Attack', 'Increase Fuel', 'Run for your life']
+            }
+        ]);
+        if (ask.opponent === 'Attack') {
+            let randomNum = Math.floor(Math.random() * 2);
+            if (randomNum > 0) {
+                p1.fuelDecrease();
+                console.log(chalk.bgGreen.white(`\n "${p1.name}" Fuel is "${p1.fuel}"`));
+                console.log(chalk.bgGreen.white(`"${o1.name}" Fuel is "${o1.fuel}" \n`));
+                if (p1.fuel <= 0) {
+                    console.log(chalk.bgBlue.white(`\n Game Over "${player1.player}"! Better Luck Next Time.... \n Thank You for playing`));
+                    process.exit();
+                }
+            }
+            if (randomNum <= 0) {
+                o1.fuelDecrease();
+                console.log(chalk.bgGreen.white(`\n ${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bgGreen.white(`${o1.name} fuel is ${o1.fuel} \n`));
+                if (o1.fuel <= 0) {
+                    console.log(chalk.bgBlue.white(`\n Congratulations "${player1.player}"! You are winner.... \n Thank You for playing \n`));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opponent === 'Increase Fuel') {
+            p1.fuelIncrease();
+            console.log(chalk.bgMagenta.white(`\n You succesfully drink the "ENERGY BOOSTER", Now your strength is "${p1.fuel}" \n`));
+        }
+        if (ask.opponent === 'Run for your life') {
+            console.log(chalk.bgRed.white("\n You Quit this game! Better Luck Next Time, \n Thank You For Choosing us"));
+            process.exit();
+        }
+    }
+    // FOR HULK
+    if (opponent1.opponent === 'Hulk') {
+        let ask = await inquirer.prompt([
+            {
+                name: "opponent",
+                type: "list",
+                message: "What would you like to do: ",
+                choices: ['Attack', 'Increase Fuel', 'Run for your life']
+            }
+        ]);
+        if (ask.opponent === 'Attack') {
+            let randomNum = Math.floor(Math.random() * 2);
+            if (randomNum > 0) {
+                p1.fuelDecrease();
+                console.log(chalk.bgGreen.white(`\n "${p1.name}" Fuel is "${p1.fuel}"`));
+                console.log(chalk.bgGreen.white(`"${o1.name}" Fuel is "${o1.fuel}" \n`));
+                if (p1.fuel <= 0) {
+                    console.log(chalk.bgBlue.white(`\n Game Over "${player1.player}"! Better Luck Next Time.... \n Thank You for playing`));
+                    process.exit();
+                }
+            }
+            if (randomNum <= 0) {
+                o1.fuelDecrease();
+                console.log(chalk.bgGreen.white(`\n ${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bgGreen.white(`${o1.name} fuel is ${o1.fuel} \n`));
+                if (o1.fuel <= 0) {
+                    console.log(chalk.bgBlue.white(`\n Congratulations "${player1.player}"! You are winner.... \n Thank You for playing \n`));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opponent === 'Increase Fuel') {
+            p1.fuelIncrease();
+            console.log(chalk.bgMagenta.white(`\n You succesfully drink the "ENERGY BOOSTER", Now your strength is "${p1.fuel}" \n`));
+        }
+        if (ask.opponent === 'Run for your life') {
+            console.log(chalk.bgRed.white("\n You Quit this game! Better Luck Next Time, \n Thank You For Choosing us"));
+            process.exit();
+        }
+    }
+} while (true);
